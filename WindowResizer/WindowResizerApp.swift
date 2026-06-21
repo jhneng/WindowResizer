6fb6a814fdb293cd9fda24d03857f9be45d5d087
Johan Engblom <johan.engblom@pm.me>
2026-06-21 14:28:06 +0300

    Initial Commit

---
+++ WindowResizer/WindowResizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowResizerApp.swift
+//  WindowResizer
+//
+//  Created by Johan Engblom on 2026-06-21.
+//
+
+import SwiftUI
+
+@main
+struct WindowResizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
